d0422620a7747cfecfd3fd11163d9d91ce4911a7
Wouter de Bruijn <wouter@hedium.nl>
2022-11-11 16:24:48 +0100

    💄 Improved styling for movies list

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,5 @@
 import { Handlers, PageProps } from "https://deno.land/x/fresh@1.1.1/server.ts";
+import { JSXInternal } from "https://esm.sh/v95/preact@10.11.0/src/jsx.d.ts";
 import { Torrent } from "../externals/Deluge.ts";
 import { Movie, QueueItem } from "../externals/Radarr.ts";
 
@@ -28,6 +29,8 @@ export const handler: Handlers<Data> = {
     const torrents = await getCurrentTorrents();
     const radarrQueue = await getCurrentQueue();
 
+    await services.sonarr.getSeries();
+
     return ctx.render({ movies, torrents, radarrQueue });
   },
 };
@@ -38,34 +41,34 @@ export default function Home({ data }: PageProps<Data>) {
   const radarrQueue = data.radarrQueue.map((item) => QueueItem(item));
 
   return (
-    <div>
+    <div class="bg-gray-900 min-h-screen">
       <div class="max-w-screen">
-        <header class="bg-green-400 flex items-center">
+        <header class="bg-green-800 flex items-center">
           <img
             src="/logo.svg"
             class="w-16 h-16 mx-2"
             alt="the fresh logo: a sliced lemon dripping with juice"
           />
-          <h1 class="font-bold">Emdi - Media Dashboard</h1>
+          <h1 class="font-bold text-white">Emdi - Media Dashboard</h1>
         </header>
       </div>
 
       <section class="mx-2 m-3">
-        <h1 class="text-xl font-bold">Latest Movies</h1>
+        <h1 class="text-xl font-bold text-white">Latest Movies</h1>
         <div class="flex flex-nowrap overflow-x-scroll">
           {movieObjects}
         </div>
       </section>
 
       <section class="mx-2 m-3">
-        <h1 class="text-xl font-bold">Current Queue</h1>
+        <h1 class="text-xl font-bold text-white">Current Queue</h1>
         <div class="flex flex-col overflow-x-scroll">
           {radarrQueue}
         </div>
       </section>
 
       <section class="mx-2 m-3">
-        <h1 class="text-xl font-bold">Current Torrents</h1>
+        <h1 class="text-xl font-bold text-white">Current Torrents</h1>
         <div class="flex flex-col overflow-x-scroll">
           {torrentObjects}
         </div>
@@ -74,19 +77,47 @@ export default function Home({ data }: PageProps<Data>) {
   );
 }
 
+function getTag(movie: Movie): JSXInternal.Element {
+  // If we have a file, then the movie is available on Emby
+  if (movie.hasFile === true) {
+    return <p class="text-green-500">On Emby</p>;
+  }
+
+  // If the movies isn't available, we wait for a release.
+  if (movie.isAvailable === false) {
+    return (
+      <p class="text-yellow-500">
+        Waiting {movie.physicalRelease.toLocaleDateString()}
+      </p>
+    );
+  }
+
+  // If the movie has only been in radarr for less than 24 hours.
+  const threshold = new Date(Date.now() - 86400000); // -24h
+  if (movie.added > threshold) {
+    return <p class="text-yellow-500">Searching</p>;
+  }
+
+  // If we don't have a file and the movie is released (metadata) we should have been downloading it.
+  return <p class="text-red-500">Failed</p>;
+}
+
 function Movie(movie: Movie) {
   return (
-    <div class="flex-none w-48 pr-2 m-2 border">
-      <img src={movie.images[0].remoteUrl} />
-      <div>
-        <h2>{movie.title}</h2>
-        <p>{movie.year}</p>
-        {movie.hasFile
-          ? <p class="text-green-500">Downloaded</p>
-          : <p class="text-red-500">Not Downloaded</p>}
-        {movie.isAvailable
-          ? <p class="text-green-500">Released</p>
-          : <p class="text-yellow-500">Not Released</p>}
+    <div class="mt-2 mr-4 bg-gray-800 rounded shadow-white">
+      <div
+        class="w-48 h-72 bg-cover rounded-t"
+        style={`background-image: url("${movie.images[0].remoteUrl}");`}
+      >
+        {/* <div class="text-white absolute top-0 left-0 w-full opacity-25">
+          <div class="bg-black p-2">
+            <h2>{movie.title}</h2>
+            <p>{movie.year}</p>
+          </div>
+        </div> */}
+      </div>
+      <div class="left-0 right-0 p-2 text-center">
+        {getTag(movie)}
       </div>
     </div>
   );
